8fb2421962508ae0b00b4a66f0f643094c700606
Sarah Schröder <saschroeder@techfak.uni-bielefeld.de>
2023-08-21 09:17:18 +0200

    test templates for typos/missing placeholders before the experiment

--- initialize_experiment.py
+++ initialize_experiment.py
@@ -8,11 +8,35 @@ from embedding import BertHuggingfaceMLM, BertHuggingface
 from unmasking_bias import PLLBias
 
 
+def test_templates(template_config):
+    target_domain = template_config['target']
+    target_words = template_config[target_domain]
+    protected_attributes = template_config['protected_attr']
+    versions_per_attr = {attr: len(template_config[attr]) for attr in protected_attributes}
+
+    # test training and tes templates
+    for template in template_config['templates_train']+template_config['templates_test']:
+        sent = template
+
+        assert target_domain in template, "did not find target placeholder in training template: "+template
+        found_attr = False
+        for attr in protected_attributes:
+            found_attr = found_attr or attr in template
+
+            if attr in template:
+                for i in range(versions_per_attr[attr]):
+                    cur_attr = attr+str(i)
+                    sent = sent.replace(cur_attr, template_config[attr][i][0])
+                assert (attr not in sent), "after replacing all attribute versions, the placeholder (" + attr\
+                                               + ")still remains:\n" + sent
+        assert found_attr, "did not find any attribute placeholder in training template: "+template
+
+
 def init(config):
 
     print("load templates and protected attributes...")
     with open(config['template_file'], 'r') as f:
-        tmp = yaml.safe_load(f)
+        template_config = yaml.safe_load(f)
 
     print("create the directory for experiment results...")
     if not os.path.isdir(config['results_dir']):
@@ -23,7 +47,7 @@ def init(config):
     with open(log_config, 'w') as file:
         yaml.dump(config, file)
 
-    #  TODO: check templates (that each one contains at least one attribute and a target placeholder (typos!)
+    test_templates(template_config)
 
     # download model if necessary
     if 'MLM' in config['objective']:  # MLM or MLM_lazy
