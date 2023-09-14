add bios dataset, debiasing, clf pipeline with debiasing, gap score as clf bias measure, fix for pll bias calculation

utils/datasets.py
@@ -9,6 +9,60 @@ from transformers import PreTrainedTokenizer
 
 from unmasking_bias import PLLBias, get_token_diffs
 
+from sklearn.utils import shuffle
+import itertools
+from collections.abc import Callable
+import numpy as np
+import math
+import pickle
+
+class BiasDataset():
+    
+    def __init__(self):
+        self.data = []
+        self.bias_types = []
+        self.groups_by_bias_types = {}
+        self.sel_bias_type = None
+        self.sel_groups = []
+        self.sel_data = []
+        self.individual_biases = []
+        self.bias_score = None # or class-wise?
+        
+    def sel_attributes(self, bias_type: str) -> bool:
+        if not bias_type in self.bias_types:
+            print("bias type", bias_type, "is not supported for this dataset")
+            return False
+        
+        self.sel_bias_type = bias_type
+        self.sel_groups = self.groups_by_bias_types[bias_type]
+        
+        self.sel_data = []
+        for sample in self.data:
+            if sample['bias_type'] == self.sel_bias_type:
+                self.sel_data.append(sample)
+        return True
+    
+    def get_neutral_samples_by_masking(self, tokenizer: PreTrainedTokenizer):
+        pass
+                    
+    # the dataset/ task specific extrinsic bias measure
+    def individual_bias(self, sample: dict):
+        pass
+        
+    def group_bias(self):
+        pass
+        
+    def compute_invidivual_extrinsic_biases(self):
+        self.individual_biases = []
+        for sample in self.sel_data:
+            score = self.individual_bias(sample)
+            self.individual_biases.append(score)
+            
+            
+################################################
+###########    CrowSPairs dataset    ###########
+################################################
+
 PUNCTUATION = string.punctuation.replace('-','')
 
 def get_group_label(modified_terms: list, bias_type: str, groups_by_bias_types: dict, terms_by_groups: dict):
@@ -55,49 +109,6 @@ def get_diff(seq1, seq2):
             
     return modified1, modified2
 
-
-class BiasDataset():
-    
-    def __init__(self):
-        self.data = []
-        self.bias_types = []
-        self.groups_by_bias_types = {}
-        self.sel_bias_type = None
-        self.sel_groups = []
-        self.sel_data = []
-        self.individual_biases = []
-        self.bias_score = None # or class-wise?
-        
-    def sel_attributes(self, bias_type: str) -> bool:
-        if not bias_type in self.bias_types:
-            print("bias type", bias_type, "is not supported for this dataset")
-            return False
-        
-        self.sel_bias_type = bias_type
-        self.sel_groups = self.groups_by_bias_types[bias_type]
-        
-        self.sel_data = []
-        for sample in self.data:
-            if sample['bias_type'] == self.sel_bias_type:
-                self.sel_data.append(sample)
-        return True
-    
-    def get_neutral_samples_by_masking(self, tokenizer: PreTrainedTokenizer):
-        pass
-                    
-    # the dataset/ task specific extrinsic bias measure
-    def individual_bias(self, sample: dict):
-        pass
-        
-    def group_bias(self):
-        pass
-        
-    def compute_invidivual_extrinsic_biases(self):
-        self.individual_biases = []
-        for sample in self.sel_data:
-            score = self.individual_bias(sample)
-            self.individual_biases.append(score)
-            
 
 class CrowSPairsDataset(BiasDataset):
     
@@ -180,12 +191,6 @@ class CrowSPairsDataset(BiasDataset):
             return
         pllBias = PLLBias(mlm.model, mlm.tokenizer, mlm.batch_size)
         
-        #stereo_more = [sample['text'] for sample in self.sel_data if sample['label'] == self.labels.index('stereo')]
-        #stereo_less = [sample['counterfactual'] for sample in self.sel_data if sample['label'] == self.labels.index('stereo')]
-        #antistereo_more = [sample['text'] for sample in self.sel_data if sample['label'] == self.labels.index('antistereo')]
-        #antistereo_less = [sample['counterfactual'] for sample in self.sel_data if sample['label'] == self.labels.index('antistereo')]
-        #self.pll_stereo_more, self.pll_stereo_less = pllBias.compare_sentence_likelihood(stereo_more, stereo_less)
-        #self.pll_anti_more, self.pll_anti_less = pllBias.compare_sentence_likelihood(antistereo_more, antistereo_less)
         sent_more = [sample['text'] for sample in self.sel_data]
         sent_less = [sample['counterfactual'] for sample in self.sel_data]
         self.pll_more, self.pll_less = pllBias.compare_sentence_likelihood(sent_more, sent_less)
@@ -202,11 +207,6 @@ class CrowSPairsDataset(BiasDataset):
                 self.individual_biases.append(self.pll_more[i]-self.pll_less[i])
             else: #antistereo
                 self.individual_biases.append(self.pll_less[i]-self.pll_more[i])
-                
-#        for i in range(len(self.pll_stereo_more)):
-#            self.individual_biases.append(self.pll_stereo_more[i]-self.pll_stereo_less[i])
-#        for i in range(len(self.pll_anti_more)):
-#            self.individual_biases.append(self.pll_anti_less[i]-self.pll_anti_more[i])
         
     def compute_group_bias(self, mlm: BertHuggingfaceMLM):
         self.compute_PLL(mlm)
@@ -218,30 +218,173 @@ class CrowSPairsDataset(BiasDataset):
                 stereo_more_likely.append(1)
             else:
                 stereo_more_likely.append(0)
-        #stereo_more_likely = [1 if self.pll_stereo_more[i] > self.pll_stereo_less[i] else 0 for i in range(len(self.pll_stereo_less))]
-        #stereo_more_likely += [1 if self.pll_anti_less[i] > self.pll_anti_more[i] else 0 for i in range(len(self.pll_anti_more))]
         
         self.bias_score = sum(stereo_more_likely)/len(stereo_more_likely)
+        
+
+        
+        
+################################################
+########        CLF Bias Measures       ########
+################################################
+
+def gap_score_single_label(y_pred: np.ndarray, y_true: np.ndarray, groups: list):
+    assert len(y_pred.shape) == 1
+    
+    n_groups = max(groups)+1
+    n_samples = y_pred.shape[0]
+    n_classes = max(y_pred)+1
+    
+    gaps = []
+    for c in range(n_classes):
+        y_pred_c = [y_pred[i] for i in range(n_samples) if y_true == c]
+        y_true_c = [y_true[i] for i in range(n_samples) if y_true == c]
+        groups_c = [groups[i] for i in range(n_samples) if y_true == c]
+        
+        group_tp = []
+        for g in range(n_groups):
+            y_pred_cg = [y_pred_c[i] for i in range(n_samples) if groups[i] == g]
+            y_true_cg = [y_true_c[i] for i in range(n_samples) if groups[i] == g]
+            tp = y_pred_cg.count(c)/len(y_true_cg)
+            group_tp.append(tp)
+        
+        if n_groups == 2:
+            gaps.append(group_tp[0]-group_tp[1])
+        else:
+            gaps.append(np.var(group_tp))
+    return gaps
+
+def gap_score_one_hot(y_pred: np.ndarray, y_true: np.ndarray, groups: list):
+    assert len(y_pred.shape) == 2
+    n_groups = max(groups)+1
+    n_samples = y_pred.shape[0]
+    n_classes = y_pred.shape[1]
+    
+    gaps = []
+    for c in range(n_classes):
+        y_pred_c = y_pred[:,c]
+        y_true_c = y_true[:,c]
+        
+        group_tp = []
+        for g in range(n_groups):
+            y_pred_cg = [y_pred_c[i] for i in range(n_samples) if groups[i] == g]
+            y_true_cg = [y_true_c[i] for i in range(n_samples) if groups[i] == g]
+            tp = len([1 for i in range(len(y_pred_cg)) if y_true_cg[i] == 1 and y_pred_cg[i] == 1])/np.sum(y_true_cg)
+            group_tp.append(tp)
+        
+        if n_groups == 2:
+            gaps.append(group_tp[0]-group_tp[1])
+        else:
+            gaps.append(np.var(group_tp))
+    return gaps
+
+
+# TODO: ROC AUC based metrics
+
+        
+        
+################################################
+###########       BIOS dataset       ###########
+################################################
+
         
         
 class BiosDataset(BiasDataset):
     
-    def __init__(self):
-        super(CrowSPairsDataset, self).__init__()
+    def __init__(self, n_folds: int, sel_labels: list, bios_file: str):
+        super(BiosDataset, self).__init__()
+        
+        with open(bios_file, 'rb') as handle:
+            data = pickle.load(handle)
+        
+        # all titles with >100 samples
+        self.labels = sel_labels
+        self.bias_types = ['gender']
+        self.groups_by_bias_types = {'gender': ['male', 'female']}
+        self.sel_bias_type = 'gender'
+        self.sel_groups = ['male', 'female']
+        
+        self.transform_data(data)
+        self.sel_data = self.data
+        
+        self.n_folds = n_folds
+        n_per_fold = math.ceil(len(self.sel_data)/n_folds)
+        self.data_folds = [self.sel_data[i:i+n_per_fold] for i in range(0, len(self.sel_data), n_per_fold)]
+        
+        self.individual_biases = []
+        self.bias_score = []
+        
+        self.train_data = []
+        self.eval_data = []
+        
+    def titles_to_one_hot(self, titles: list):
+        one_hot = np.zeros(len(self.labels))
+        for title in titles:
+            if not title in self.labels:
+                continue
+            one_hot[self.labels.index(title)] = 1
+        return one_hot
+            
+    def transform_data(self, data):
+        self.data = []
+        for sample in data:
+            if not sample['valid']:
+                continue
+
+            label = self.titles_to_one_hot(sample['titles'])
+            if np.sum(label) == 0: # we COULD use those samples anyway...
+                continue
+
+            group = self.sel_groups.index('male')
+            if sample['gender'] == 'F':
+                group = self.sel_groups.index('female')
+
+            new_sample = {'id': sample['Unnamed: 0'], 'text': sample['raw'][sample['start_pos']:], 'counterfactual': sample['bio'], 'label': label, 
+                          'bias_type': 'gender', 'group': group}
+            self.data.append(new_sample)
+        self.data = shuffle(self.data, random_state=0)
+            
+    def sel_attributes(self, bias_type: str) -> bool:
+        if not bias_type in self.bias_types:
+            print("bias type", bias_type, "is not supported for this dataset")
+            return False
+        # only bias type was already selected in __init__
+        return True
+    
+    def set_data_split(self, fold_id):
+        assert fold_id >= 0 and fold_id < self.n_folds
+        
+        self.eval_data = self.data_folds[fold_id]
+        self.train_data = list(itertools.chain.from_iterable([fold for i, fold in enumerate(self.data_folds) if i != fold_id]))
     
     def individual_bias(self, sample: dict):
         pass
         
         # BIOS:
         # ?? counterfactual prediction diff/ bool does prediction change
-        
     
-    def group_bias(self):
+    def group_bias(self, prediction_wrapper: Callable):
         pass
         
-        # BIOS (clf):
-        # GAP (diff) or variance of class-wise probabilities over protected groups
-        # maybe AUC
+        y_true = [sample['label'] for sample in self.eval_data]
+        groups = [sample['group'] for sample in self.eval_data]
+        sent = [sample['text'] for sample in self.eval_data]
+        
+        y_pred = prediction_wrapper(sent)
+        
+        gaps = gap_score_one_hot(y_pred, np.asarray(y_true), groups)
+        self.bias_score = gaps # class-wise gaps
+        
+    def roc_auc_bias(self):
+        pass
+        #TODO
+        
+        
+        
+
+#################################################
+#########    Jigsaw toxicity dataset    #########
+#################################################
         
         
 class JigsawDataset(BiasDataset):
@@ -258,7 +401,8 @@ class JigsawDataset(BiasDataset):
     
     def group_bias(self):
         pass
-        
-        # Jigsaw (regression):
-        # subgroup AUC (vs. bias AUC)
         # maybe also GAP-like score?
+        
+    def roc_auc_bias(self):
+        pass
+        #TODO

utils/models.py
@@ -6,6 +6,65 @@ import torch
 from torch import Tensor
 from torch.utils.data import DataLoader, TensorDataset
 import torch.nn.functional as F
+from sklearn.decomposition import PCA
+from embedding import BertHuggingfaceMLM
+
+class Debias():
+
+    def __init__(self):
+        self.pca = None
+        self.n_attributes = 1
+
+    def sub_mean(self, pairs):
+        means = np.mean(pairs, axis=0)
+        for i in range(means.shape[0]):
+            for j in range(pairs.shape[0]):
+                pairs[j,i,:] = pairs[j,i,:] - means[i]
+        return pairs
+
+    def flatten(self, pairs):
+        flattened = []
+        for pair in pairs:
+            for vec in pair:
+                flattened.append(vec)
+        return flattened
+
+    def drop(self, u, v):
+        return u - v * u.dot(v) / v.dot(v)
+
+    def get_bias(self, u, V):
+        norm_sqrd = np.sum(V * V, axis=-1)
+        vecs = np.divide(V @ u, norm_sqrd)[:, None] * V
+        subspace = np.sum(vecs, axis=0)
+        return subspace
+
+    def dropspace(self, u, V):
+        return u - self.get_bias(u, V)
+
+    def normalize(self, vectors: np.ndarray):
+        norms = np.apply_along_axis(np.linalg.norm, 1, vectors)
+        vectors = vectors / norms[:, np.newaxis]
+        return np.asarray(vectors)
+
+    def fit(self, embedding_tuples: list):
+        self.n_attributes = len(embedding_tuples)
+        
+        embedding_tuples = self.normalize(np.asarray(embedding_tuples))
+        assert embedding_tuples.shape[0] < embedding_tuples.shape[1], "the number of samples should be larger than the number of bias attributes"
+        print("fit pca for debiasing")
+        encoded_pairs = self.sub_mean(embedding_tuples)
+        flattened = self.flatten(encoded_pairs)
+        self.pca = PCA()
+        self.pca.fit(flattened)
+        print("explained variance of the first 20 principal components:")
+        print(self.pca.explained_variance_ratio_[:20])
+
+    def predict(self, emb, k=3):
+        print("Debias: predict with k="+str(k))
+        X = self.normalize(emb)
+        debiased = [self.dropspace(X[i,:], self.pca.components_[:k]) for i in range(X.shape[0])]
+        return self.normalize(np.asarray(debiased))
+    
 
 class CLFHead(torch.nn.Module):
     
@@ -24,6 +83,7 @@ class CLFHead(torch.nn.Module):
         x = self.softmax(x)
         return x
     
+    
 class SimpleCLFHead(torch.nn.Module): # this copies the BertForSequenceClassificationHead
     
     def __init__(self, input_size: int, output_size: int, dropout_prob=0.1):
@@ -39,6 +99,7 @@ class SimpleCLFHead(torch.nn.Module): # this copies the BertForSequenceClassific
         x = self.softmax(x)
         return x
     
+    
 class MLMHead(torch.nn.Module):
     def __init__(self, input_size: int, pretrained_head: torch.nn.Module):
         super().__init__()
@@ -51,7 +112,7 @@ class MLMHead(torch.nn.Module):
         x = self.softmax(x)
         return x
 
-# TODO: also want sklearn clf?
+    
 class CustomModel():
 
     def __init__(self, parameters: dict, model: torch.nn.Module):
@@ -59,7 +120,7 @@ class CustomModel():
         self.batch_size = parameters['batch_size']
         self.criterion = parameters['criterion']
         self.lr = parameters['lr']
-        self.optimizer = parameters['optimizer'](params=model.parameters(), lr=self.lr)
+        self.optimizer = parameters['optimizer'](params=self.model.parameters(), lr=self.lr)
         
         if torch.cuda.is_available():
             self.model = self.model.to('cuda')
@@ -126,3 +187,38 @@ class CustomModel():
         torch.cuda.empty_cache()
         return np.vstack(predictions)
     
+
+class DebiasPipeline():
+    
+    def __init__(self, parameters: dict, embedder: BertHuggingfaceMLM, head: torch.nn.Module, debias = False):
+        self.clf = CustomModel(parameters, head)
+        self.debiaser = None
+        if debias:
+            self.debiaser = Debias()
+        self.debias_k = parameters['debias_k']
+        self.embedder = embedder
+        
+    def fit(self, X: list, y: list, group_label: list = None, epochs=2):
+        print("embed samples...")
+        emb = self.embedder.embed(X)
+        
+        if self.debiaser is not None and group_label is not None:
+            print("fit and apply debiasing...")
+            emb_per_group = []
+            n_groups = max(group_label)+1
+            for group in range(n_groups):
+                emb_per_group.append([e for i, e in enumerate(emb) if group_label[i] == group])
+            self.debiaser.fit(emb_per_group)
+            emb = self.debiaser.predict(emb, self.debias_k)
+        
+        print("fit clf head...")
+        head.fit(emb, y, epochs=epochs)
+    
+    def predict(self, X: list):
+        emb = self.embedder.embed(X)
+        
+        if self.debiaser is not None and self.debiaser.pca is not None:
+            emb = self.debiaser.predict(emb, k=self.debias_k)
+        
+        pred = head.predict(emb)
+        return pred