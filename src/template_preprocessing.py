13d017a38b8d1b2956726f83142b7ee3acb61b3b
Sarah Schröder <saschroeder@techfak.uni-bielefeld.de>
2026-06-23 09:49:18 +0200

    add script to preprocess templates (count attribute keys, sample train/val/test split)

---
+++ src/template_preprocessing.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import yaml
+
+import numpy as np
+
+TEMPLATE_FILE = 'data/templates_minimal.yaml'
+
+# TODO: should the validation split just be a copy of train (without the multi-key samples?)
+def custom_split(samples_train_only, samples_test, percent_test=0.4): #, percent_val=0.2):
+    """
+    Splits data into Train, Validation, and Test sets following specific rules:
+    - Test and validation samples are only taken from 'samples_test'
+    - The test set size is specified via 'percent_test' and the training set is the rest
+    - The validation set is the training set excluding samples from 'samples_train_only' - intended to distinguish generalization and overfitting
+    """
+    
+    # determine total rows and split sizes
+    total_rows = len(samples_train_only) + len(samples_test)
+    
+    if total_rows == 0:
+        raise ValueError("No data available in either dataframe.")
+
+    assert (0.0 < percent_test < 1.0)
+    #assert (0.0 < percent_val < 1.0) 
+    #n_val = int(np.round(total_rows * percent_val))
+    n_test = int(np.round(total_rows * percent_test))
+    n_train = len(df) - n_test # - n_val
+    
+    # check sufficent test/val samples
+    percent_train = 1.0 - percent_test #- percent_val
+    #assert (percent_train < 1.0 and percent_train > 0)
+    if len(samples_train_only) > n_train:
+        print("warning: training set will be larger than requested (%.2f) (too many samples not eligible for test/val split)" % (percent_train))
+
+    # shuffle
+    shuffled_test = samples_test.sample(frac=1, random_state=42).reset_index(drop=True)
+
+    # extract test and validation split first
+    df_test = shuffled_test.iloc[:n_test]
+    #df_val = shuffled_test.iloc[n_test : n_test + n_val]
+    
+    # train set is the remaining samples from 'samples_test' plus 'samples_train_only', shuffle again
+    #df_test_remainder = shuffled_test.iloc[n_test + n_val :]
+    df_test_remainder = shuffled_test.iloc[n_test :]
+    df_val = df_test_remainder
+    df_train = pd.concat([samples_train_only, df_test_remainder], ignore_index=True)
+    df_train = df_train.sample(frac=1, random_state=42).reset_index(drop=True)
+
+    return df_train, df_val, df_test
+
+
+# load yaml with attr/ target keys and csv with templates
+with open(TEMPLATE_FILE, 'r') as f:
+    template_config = yaml.safe_load(f)
+
+df = pd.read_csv('data/templates.csv', sep=';')
+
+
+# get templates, attribute and target keys
+templates = list(df['template'])
+attributes = template_config['protected_attr']
+keys_by_attr = {attr: template_config[attr]['KEYS'] for attr in attributes}
+target = template_config['target']
+
+
+# assert every template has a target key
+for template in templates:
+    assert target in template, "found template without target key: "+template
+
+for i, template in enumerate(templates):
+    for attr in attributes:
+        df.loc[i, attr] = 0
+        for key in keys_by_attr[attr]:
+            df.loc[i, attr] += template.count(key)
+
+#print(df)
+condition_test = df[attributes] <= 1
+condition_not_test = df[attributes] > 1
+samples_train_only = df[condition_not_test.any(axis=1)]
+samples_test_valid = df[condition_test.all(axis=1)]
+
+print("got %i samples in total" % len(df))
+print("found %i samples with more than one key per attribute (not eligible for test/val split)" % len(samples_train_only))
+
+# split data
+df_train, df_val, df_test = custom_split(samples_train_only, samples_test_valid, percent_test=0.4)
+
+
+for attr in attributes:
+    print("for %s got %i / %i / %i (train/val/test) samples" % (attr, len(df_train[df_train[attr] >= 1]), len(df_val[df_val[attr] >= 1]), len(df_test[df_test[attr] >= 1])))
+
+
+template_config['templates_train'] = list(df_train['template'])
+template_config['templates_val'] = list(df_val['template'])
+template_config['templates_test'] = list(df_test['template'])
+
+with open(TEMPLATE_FILE, 'w') as f:
+    yaml.dump(template_config, f, default_flow_style=False, sort_keys=False)
