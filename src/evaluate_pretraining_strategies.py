change data stat normalization (to fit p(g|t,a)); fix missing labels in random masked sentences

src/evaluate_pretraining_strategies.py
@@ -182,14 +182,26 @@ def create_dataset(data_path: str, stat_path: str, tokenizer, template_config: d
 
 
         df_data_stats = pd.DataFrame(data=target_group_occ).astype(float)
+        df_data_stats.to_csv(stat_path.replace('.csv', '_raw.csv'), index_label='groups')
 
-        # normalize per group ( -> p(target | group))
+        print(df_data_stats)
+
+        # normalize per target and attribute ( -> p(group | target, attr))
+        for attr in protected_attributes:
+            cur_groups = template_config[attr]['GROUPS'][1:]
+            for target in target_words:
+                sel = df_data_stats.loc[cur_groups, target]
+                sel_sum = np.sum(sel)
+                df_data_stats.loc[cur_groups, target] /= sel_sum
+        """
         for group in group_list:
             # overall occurence of this group
             sel = df_data_stats.loc[group, :]
             sel_sum = np.sum(sel)
             df_data_stats.loc[group, :] /= sel_sum
+        """
 
+        
         df_data_stats.to_csv(stat_path, index_label='groups')
     else:
         print("load training data from "+data_path)

src/utils/mlm.py
@@ -44,6 +44,8 @@ def apply_random_masking(input_ids: torch.Tensor, mask_token_id: int, vocab_size
     # apply only to tokens specified in token_mask
     rand_probs = torch.rand_like(input_ids.float())
     to_mask = token_mask & (rand_probs < 0.15)
+    labels = input_ids.clone()
+    labels[~to_mask] = -100
 
     split_rand = torch.rand_like(input_ids.float())
     is_mask = to_mask & (split_rand < 0.80)
@@ -55,7 +57,7 @@ def apply_random_masking(input_ids: torch.Tensor, mask_token_id: int, vocab_size
         random_tokens = torch.randint(0, vocab_size, (len(rows),), device='cpu')
         input_ids[rows, cols] = random_tokens
 
-    return input_ids
+    return input_ids, labels
 
 
 
@@ -89,7 +91,7 @@ def mask_texts(bert: BertHuggingfaceMLM, texts: list[str], max_length = 512, ret
         ], device='cpu')
     
     candidate_mask = (~special_tokens_mask.bool()) & (attention_mask.bool())
-    inputs['input_ids'] = apply_random_masking(input_ids, bert.tokenizer.mask_token_id, len(bert.tokenizer), candidate_mask, mask_prob=0.15)
+    inputs['input_ids'], inputs['labels'] = apply_random_masking(input_ids, bert.tokenizer.mask_token_id, len(bert.tokenizer), candidate_mask, mask_prob=0.15)
 
     if return_tokens:
         return inputs